a6b99d69b09ea2678db3012c316e3555ca432b5c
yellowflash104 <udit.sharma104@gmail.com>
2021-03-19 01:06:17 +0530

    configuring settings.py file

--- selfesteem/settings.py
+++ selfesteem/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-print(BASE_DIR)
+#print(BASE_DIR)
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-print(STATIC_ROOT)
+#print(STATIC_ROOT)
